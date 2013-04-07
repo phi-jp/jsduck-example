280a2905c92738d247eeea7687b66a3613ffb2de
phi <phi1618jp@gmail.com>
2013-04-08 02:19:16 +0900

    initial commit

---
+++ src/example.js
@@ -0,0 +1,16 @@
+
+/**
+ * @class tm.ExampleClass
+ * example class
+ */
+
+
+
+/**
+ * @method area
+ * Returns area of a circle
+ * @param {Number} r Radius of the circle.
+ * @return {Number} The area
+ */
+
+
